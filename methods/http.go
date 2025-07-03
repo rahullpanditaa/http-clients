3fb49889ee7cdebabbc87019b106153a46202e10
Rahul Pandita <rpandita1999@outlook.com>
2025-07-03 05:43:27 +0000

    create function to send a post request

--- methods/http.go
+++ methods/http.go
@@ -1,22 +1,47 @@
 package methods
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
 )
 
-func getusers(url string) ([]User, error) {
-	resp, err := http.Get(url) // recieve an http json response
+func createUser(url, apiKey string, data User) (User, error) {
+	// data needs to be sent as JSON
+	// encode it as JSON first
+	jsonData, err := json.Marshal(data)
 	if err != nil {
-		return nil, err
+		return User{}, err
 	}
 
-	var users []User
-	decoder := json.NewDecoder(resp.Body)
-
-	if err := decoder.Decode(&users); err != nil {
-		return nil, err
+	// now, data is ready as json
+	// create a new request
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return User{}, err
 	}
 
-	return users, nil
+	// request headers
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-API-Key", apiKey)
+
+	// create client, make the request
+	client := &http.Client{}
+	res, err := client.Do(req) // the http request created
+	// returns an http response and an error
+	if err != nil {
+		return User{}, err
+	}
+	// now have the http response as json data
+	defer res.Body.Close() // close response body after
+	// function returns
+
+	// decode json data
+	var user User
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&user); err != nil {
+		return User{}, err
+	}
+
+	return user, nil
 }
